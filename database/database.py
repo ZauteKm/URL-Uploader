6987984c0f8a13d90ec942413a93a3eb61fccf36
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-02-11 13:09:12 +0530

    Error?

--- database/database.py
+++ database/database.py
@@ -9,7 +9,7 @@ class Database:
 
     def __init__(self, uri, database_name):
         self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.clinton = self._client[database_name]
+        self.db = self._client[database_name]
         self.col = self.db.USERS
 
     def new_user(self, id):
