a34db1e190868ec89d13a9cf98b7383155a6726a
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-02-11 12:37:01 +0530

    Update help_Nekmo_ffmpeg.py

--- helper_funcs/help_Nekmo_ffmpeg.py
+++ helper_funcs/help_Nekmo_ffmpeg.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K & Zaute Km
+
+# the logging things
 import logging
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
