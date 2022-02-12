449746afe7cc1d3af305cc73a2252de96f6c6d1e
Zaute Km <77600757+ZauteKm@users.noreply.github.com>
2022-02-12 13:36:36 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -3,13 +3,13 @@
 # (c) Zaute Km
 
 from pyrogram import Client, filters
-from config import AUTH_USERS
+from config import Config
 from database.database import db
 
 
 @Client.on_message(filters.private & filters.command('total'))
 async def sts(c, m):
-    if m.from_user.id != AUTH_USERS:
+    if m.from_user.id != Config.AUTH_USERS:
         return 
     total_users = await db.total_users_count()
     await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
